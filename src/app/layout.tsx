4c1921ea6b2511cc4ca3bef178a9a8c5f7cd21ef
JChartier001 <JChartier001@gmail.com>
2024-06-21 21:52:56 -0400

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google';
+import ContextProviders from '@/providers';
+import "./globals.css";
+
+const poppins = Poppins({
+	weight: ['400', '500', '700'],
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+});
+
+
+export const metadata: Metadata = {
+	title: 'Doctrina LMS',
+	description: 'All-in-one aesthetics education platform',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+		<html lang='en'>
+			<ContextProviders>
+				<body className={poppins.className}>{children}</body>
+			</ContextProviders>
+		</html>
+	);
+}
